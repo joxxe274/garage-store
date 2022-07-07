9f40c24c1e99ec2c9269c5161550d214dde99b6f
Jorge Sotomayor <joxxe274@gmail.com>
2022-07-07 12:13:37 -0600

    feat: testing image share

--- layouts/Main.layout.tsx
+++ layouts/Main.layout.tsx
@@ -22,7 +22,7 @@ export const MainLayout: React.FC<Props> = ({
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         {/* social meta tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="Joxxe's garage sales" />
+        <meta name="twitter:site" content={title} />
         <meta name="twitter:creator" content="@joxxe274" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
@@ -31,9 +31,9 @@ export const MainLayout: React.FC<Props> = ({
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
-        <meta property="og:url" content={process.env.SELF_URL} />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_SELF_URL} />
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Creatorland" />
+        <meta property="og:site_name" content="Joxxe's garage sale." />
         <meta property="og:locale" content="en_US" />
         <meta property="og:locale:alternate" content="en_US" />
       </Head>
